6b55e763175c72e6a7ebcfe799392a46b7939edc
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2022-03-16 14:31:55 +0100

    FindLabeledDocuments.ps1

---
+++ FindLabeledDocuments.ps1
@@ -0,0 +1,35 @@
+﻿<#
+FindLabeledDocuments
+Version: 20220316
+Christoph Hannebauer, glueckkanja-gab AG <christoph.hannebauer@glueckkanja-gab.com>
+
+Searches a given path for all files that are labeled with a specific Sensitivity Label
+
+	.PARAMETER Path
+    The path to search in
+	
+	.PARAMETER Labels
+    A list of labels to search for
+
+	.EXAMPLE
+    .\FindLabeledDocuments.ps1 -Path "C:\" -Labels "Confidential", "Strictly Confidential" 1>businessfiles.txt 2>error.txt
+
+#>
+param
+( 
+[Parameter(Position=0,Mandatory=$true,ValueFromPipeline=$false,HelpMessage='The path to search in')][string]$Path,
+[Parameter(Position=1,Mandatory=$true,ValueFromPipeline=$false,HelpMessage='A list of labels to search for')][string[]]$Labels
+)
+
+function outputFileIfLabeled($status) {
+    if ($Labels.Contains($status.MainLabelName)) {
+        "$($status.MainLabelName): $($status.FileName)"
+    }
+}
+
+$aipErrors = @()
+Get-AIPFileStatus -Path $Path -ErrorVariable +aipErrors -ErrorAction SilentlyContinue | Where-Object { outputFileIfLabeled($_) }
+
+foreach ($errorAccess in $aipErrors) {
+    "$($errorAccess.CategoryInfo.Reason): $($errorAccess.CategoryInfo.TargetName)"
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # LabelingTools
+
 Tools that help working with Microsoft Sensitivity Labels
+
+## FindLabeledDocuments.ps1
+
+Searches a given path for all files that are labeled with a specific Sensitivity Label. For encrypted documents, the user must have decryption rights in order to display the specific label. Files that cannot be accessed are also listed as such, including those with missing decryption rights.
+
+### Example
+
+```
+.\FindLabeledDocuments.ps1 -Path "C:\" -Labels "Confidential", "Strictly Confidential" 1>businessfiles.txt
+```
+
+## License
+
+All tools are available under the terms of the [MIT LICENSE](LICENSE).
